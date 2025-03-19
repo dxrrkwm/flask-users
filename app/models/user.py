64d3eb0a3b3a70f7a29d5fd824fab0774cf18c0e
Vadym Sulim <ohnooq1@gmail.com>
2025-03-19 04:28:42 +0200

    updated user model to use sqla 2.0

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,8 @@
 from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.extensions import db
 
@@ -6,10 +10,10 @@ from app.extensions import db
 class User(db.Model):
     __tablename__ = "users"
 
-    id: int = db.Column(db.Integer, primary_key=True)
-    name: str = db.Column(db.String(100), nullable=False)
-    email: str = db.Column(db.String(120), unique=True, nullable=False)
-    created_at: datetime = db.Column(db.DateTime, default=datetime.now)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
 
     def __init__(self, name: str, email: str) -> None:
         self.name = name
